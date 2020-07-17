430a75110cd37d3fcdf14855a846c808a86ed83a
Jeroen P. Broks <facebook@tbbs.nl>
2020-07-17 09:40:18 +0200

    Workout!

---
+++ QuickNeil.cs
@@ -0,0 +1,43 @@
+// Lic:
+// Quick Neil
+// Main Portion
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 2020
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 20.07.17
+// EndLic
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickNeil {
+    class QuickNeil {
+        static void Main(string[] args) {
+            // The code provided will print ???Hello World??? to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
